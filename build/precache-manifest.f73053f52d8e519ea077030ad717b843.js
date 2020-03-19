602eb82d8d396d12b8b4fb4c38f7a58f7525b794
Muhammed Mazen Hafez <mhafez@apper.tech>
2020-03-19 20:37:19 +0200

    Update React Frontend/Add Endpoints

---
+++ build/precache-manifest.f73053f52d8e519ea077030ad717b843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1fb40760201c66dcbda3ec63343e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8855498c1794b958bb3e",
+    "url": "/static/css/2.7b23983f.chunk.css"
+  },
+  {
+    "revision": "92150ba521a3546912bd",
+    "url": "/static/css/main.0af62a06.chunk.css"
+  },
+  {
+    "revision": "8855498c1794b958bb3e",
+    "url": "/static/js/2.27448556.chunk.js"
+  },
+  {
+    "revision": "869dbe2dbebfd751bcd9d0e1ddf9b780",
+    "url": "/static/js/2.27448556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92150ba521a3546912bd",
+    "url": "/static/js/main.970c97cd.chunk.js"
+  },
+  {
+    "revision": "a6bc9bf3634dbf174d58",
+    "url": "/static/js/runtime-main.2c723821.js"
+  }
+]);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+var dataPath = "./data/cases.json";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+router.get("/api/data", function(req, res, next) {
+  if (req.header("secret") == "CoolSecret123") {
+    let cases = JSON.parse(fs.readFileSync(dataPath));
 
+    res.json(cases);
+  } else {
+    res.json({ result: "You don't have permission to access this view!" });
+  }
+});
+router.post("/api/data", function(req, res, next) {
+  if (req.header("secret") == "CoolSecret123") {
+    let data = JSON.stringify(req.body);
+    fs.writeFileSync(dataPath, data);
+
+    let cases = JSON.parse(fs.readFileSync(dataPath));
+
+    res.json(cases);
+  } else {
+    res.json({ result: "You don't have permission to access this view!" });
+  }
+});
 module.exports = router;
